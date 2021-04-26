2db0186b0d92b92357a6ba735807e10a2a27cd64
Sharvetha <Sharvetha@10.19.100.127>
2021-04-26 12:55:25 +0800

    Exercise 4.1

---
+++ Exercise fruit/src/FRUIT/Greenapple.java
@@ -0,0 +1,37 @@
+package FRUIT;
+
+public class Greenapple extends Apple {
+	
+	private String place;
+	private String season;
+	
+	public Greenapple(String n , int q , double p, String g , String s) {
+		super(n ,q , p);
+		this.place = g;
+		this.season = s;
+	    }
+		
+
+	public String Growingplace() {
+	return this.place;
+	}
+	
+	public String Season() {
+	return this.season;
+	}
+	
+    public double caltotalprice() {
+	return this.price * this.quantity ;
+    }
+	
+	
+	public String toString() {
+        return super.toString()+"\nQuantity = "+ quantity+
+        		                 "\nPrice per Kg = RM"+ price+
+        		                 "\nMoon Drops will grow at : " + place +
+        		                 "\nSeason : " + season +
+        		                 "\nTotal Price= RM"+caltotalprice();
+        	
+}
+}
+
